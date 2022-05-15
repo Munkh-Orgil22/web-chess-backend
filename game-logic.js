ef99b112e59a5e37ac7788db37bc1d0be024ef73
Munkh-Orgill <mnkhrglkd@gmail.com>
2022-05-15 17:15:28 +0800

    update

--- game-logic.js
+++ game-logic.js
@@ -2,7 +2,7 @@
 var io
 var gameSocket
 
-var gamesInSession = []
+var gamesIn = []
 
 
 const initializeGame = (sio, socket) => {
@@ -46,20 +46,17 @@ function videoChatBackend() {
 
 
 function playerJoinsGame(idData) {
-    /**
-     * Joins the given socket to a session with it's gameId
-     */
 
-    // A reference to the player's Socket.IO socket object
+
     var sock = this
 
-    // Look up the room ID in the Socket.IO manager object.
+
     var room = io.sockets.adapter.rooms[idData.gameId]
     // console.log(room)
 
-    // If the room exists...
+
     if (room === undefined) {
-        this.emit('status', "This game session does not exist.");
+        this.emit('status', "Тоглолт үүсээгүй байна.");
         return
     }
     if (room.length < 2) {
@@ -75,31 +72,26 @@ function playerJoinsGame(idData) {
             io.sockets.in(idData.gameId).emit('start game', idData.userName)
         }
 
-        // Emit an event notifying the clients that the player has joined the room.
+
         io.sockets.in(idData.gameId).emit('playerJoinedRoom', idData);
 
     } else {
-        // Otherwise, send an error message back to the player.
-        this.emit('status', "There are already 2 people playing in this room.");
+
+        this.emit('status', "Энэ линкээр аль хэдийн 2 хүн тоглож байна.");
     }
 }
 
 
 function createNewGame(gameId) {
-    // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
+
     this.emit('createNewGame', { gameId: gameId, mySocketId: this.id });
 
-    // Join the Room and wait for the other player
+
     this.join(gameId)
 }
 
 
 function newMove(move) {
-    /**
-     * First, we need to get the room ID in which to send this message. 
-     * Next, we actually send this message to everyone except the sender
-     * in this room. 
-     */
 
     const gameId = move.gameId
 
